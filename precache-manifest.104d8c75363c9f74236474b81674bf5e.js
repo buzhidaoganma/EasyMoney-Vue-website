0e40fc0c1ba63baa0d4744c7ac11481e543259e0
jed <2678646597@qq.com>
2022-08-06 02:08:37 +0800

    deploy

---
+++ precache-manifest.104d8c75363c9f74236474b81674bf5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7637b9f57baefd95a51e",
+    "url": "/EasyMoney-Vue-website/css/app.adb88145.css"
+  },
+  {
+    "revision": "d989c63bf6b410ee1601e271e2767a7c",
+    "url": "/EasyMoney-Vue-website/index.html"
+  },
+  {
+    "revision": "7637b9f57baefd95a51e",
+    "url": "/EasyMoney-Vue-website/js/app.4e66023c.js"
+  },
+  {
+    "revision": "dc8fccd2977f144af87c",
+    "url": "/EasyMoney-Vue-website/js/chunk-vendors.6a74cd55.js"
+  },
+  {
+    "revision": "38c86ee580c0f0227a68bad3d0663039",
+    "url": "/EasyMoney-Vue-website/manifest.json"
+  },
+  {
+    "revision": "024b40fcde964078553c0697de8b4961",
+    "url": "/EasyMoney-Vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/EasyMoney-Vue-website/robots.txt"
+  }
+]);
